77969613258d5216c29c82cb9539a0ead8f3faab
ramiwzayed <ramiwzayed@gmail.com>
2026-01-10 15:16:15 +0200

    Add BubbleSort component with basic structure and test
    This commit introduces the BubbleSort component, including its HTML, CSS, and TypeScript files. It also adds a unit test to verify the component is created successfully. The implementation currently has a placeholder structure.

---
+++ sorts/src/app/bubble-sort/bubble-sort.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bubble-sort',
+  imports: [],
+  templateUrl: './bubble-sort.html',
+  styleUrl: './bubble-sort.css',
+})
+export class BubbleSort {
+
+}
