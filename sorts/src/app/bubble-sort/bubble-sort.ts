d427325845354e0ced19a6148551742f3ae9c451
ramiwzayed <ramiwzayed@gmail.com>
2026-01-10 15:29:56 +0200

    Add Bubble Sort implementation with UI enhancements
    Replaced placeholder text with a heading and a sort button in the HTML. Implemented the Bubble Sort algorithm in the component and initialized it on component load with a sample array.

--- sorts/src/app/bubble-sort/bubble-sort.ts
+++ sorts/src/app/bubble-sort/bubble-sort.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bubble-sort',
-  imports: [],
-  templateUrl: './bubble-sort.html',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
   styleUrl: './bubble-sort.css',
 })
-export class BubbleSort {
+export class AppComponent implements OnInit {
 
+  arr: number[] = [52, 14, 45, 21, 87];
+
+  ngOnInit() {
+    this.bubbleSort();
+    console.log(this.arr);
+  }
+
+  bubbleSort() {
+    let n = this.arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (this.arr[j] > this.arr[j + 1]) {
+          let temp = this.arr[j];
+          this.arr[j] = this.arr[j + 1];
+          this.arr[j + 1] = temp;
+        }
+      }
+    }
+  }
 }
