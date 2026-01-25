7a5e81add9c5ae56709f602e397a80b8a1312df1
ramiwzayed <ramiwzayed@gmail.com>
2026-01-26 01:08:52 +0200

    Add MergeSort component with basic structure and test
    Introduce the MergeSort component with HTML, CSS, and TypeScript files. Includes a unit test to verify the component's creation. This serves as the foundational setup for the merge-sort feature.

---
+++ sorts/src/app/merge-sort/merge-sort.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merge-sort',
+  imports: [],
+  templateUrl: './merge-sort.html',
+  styleUrl: './merge-sort.css',
+})
+export class MergeSort {
+
+}
