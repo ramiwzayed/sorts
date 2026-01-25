7ca26a9a76707e90fa9d8045e9412b019b5346d2
ramiwzayed <ramiwzayed@gmail.com>
2026-01-26 01:18:19 +0200

    Add MergeSortModule with initial app component
    Introduce the MergeSortModule containing a basic AppComponent. This sets up the foundational structure for the merge sort feature in the application.

---
+++ sorts/src/app/merge-sort/merge-sort-module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<div>App Component</div>'
+})
+export class AppComponent {
+  title = 'Merge Sort';
+}
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MergeSortModule { }
