14b9bbb3f3327b33fedba9da9e04e90588e0d207
ramiwzayed <ramiwzayed@gmail.com>
2026-01-28 00:26:21 +0200

    Add initial HeapSort component and module structure
    Introduced the HeapSort component with basic template, styles, and a spec file for testing. Also created a module for the component, laying the foundation for future development.

---
+++ sorts/src/app/heap-sort/heap-sort.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heap-sort',
+  imports: [],
+  templateUrl: './heap-sort.html',
+  styleUrl: './heap-sort.css',
+})
+export class HeapSort {
+
+}
