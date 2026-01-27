3bb6f479498a201bf98b1067fa96c8ac90c09478
ramiwzayed <ramiwzayed@gmail.com>
2026-01-27 23:55:29 +0200

    Refactor selection sort component for standalone use.
    Added `standalone: true` to make the component independently usable and imported `CommonModule` for Angular features. Improved variable declarations for better code readability and adjusted the template to enhance output clarity.

--- sorts/src/app/selection-sort/selection-sort.ts
+++ sorts/src/app/selection-sort/selection-sort.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {throwIfEmpty} from 'rxjs';
 
 @Component({
   selector: 'app-selection-sort',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './selection-sort.component.html'
 })
 export class SelectionSortComponent {
   array: number[] = [64, 25, 12, 22, 11];
+  i: any;
 
   selectionSort() {
     const num = this.array.length;
@@ -18,12 +23,10 @@ export class SelectionSortComponent {
           minIdx = j;
         }
       }
-      let temp = this.array[minIdx];
+
+      const temp = this.array[minIdx];
       this.array[minIdx] = this.array[i];
       this.array[i] = temp;
     }
   }
-}
-
-export class SelectionSort {
 }
